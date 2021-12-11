cd01956e44c460298475aac9dafbccd70ff47140
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2021-12-11 09:45:17 +0700

    perubahan

---
+++ Assets/script/SpawnObstacles.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObstacles : MonoBehaviour
+{
+    public GameObject obstacle;
+    public float maxX;
+    public float minX;
+    public float maxY;
+    public float minY;
+    public float timeBetweenSpawn;
+    private spawnTime;
+
+    
+    void Update()
+    {
+        if(Time.time > spawntime)
+        {
+            Spawn();
+            spawntime = Time.time + timeBetweenSpawn;
+        }
+    }
+
+    
+    void Spawn()
+    {
+        float randomX = randomX.Range(minX, maxX);
+        float randomY = randomY.Range(minY, maxY);
+
+        Instantiate(obstacle, transform.position + new Vector3(randomX, randomY, 0).transform.rotation);
+    }
+}
